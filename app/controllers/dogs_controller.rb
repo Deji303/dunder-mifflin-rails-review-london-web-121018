968b87a7c3ea40cd0069f7f7f0495be8dd251d8a
github username <github email address>
2019-01-16 16:09:13 +0000

    learn submit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,12 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+    @scooby = @dogs.each.sort_by{|d| d.employees.count}
+
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    # byebug
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,42 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params(:first_name,:last_name,:title,:alias,:office,:dog_id))
+
+    if @employee.valid?
+      @employee = Employee.create(employee_params(:first_name,:last_name,:title,:alias,:office,:dog_id))
+      redirect_to @employee
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params(:first_name,:last_name,:title,:alias,:office,:dog_id))
+    if @employee.valid?
+      redirect_to @employee
+    else
+      render 'edit'
+    end
+  end
+
+  def employee_params(*args)
+    params.require(:employee).permit(args)
+  end
 end
